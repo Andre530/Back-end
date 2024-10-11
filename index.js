9f1d7bf4c8200f64cb48707ea4d2bdbfef7bdfd3
Andre530 <dallgnolfaustinoa@gmail.com>
2024-10-11 14:07:25 -0300

    .
    .

--- index.js
+++ index.js
@@ -1,45 +1,106 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 const porta = 3000;
 
-app.arguments(cors());
-app.arguments(express.json());
+app.use(cors());
+app.use(express.json());
 
-const vetor = [
-    { mensagem: 'oi', numero: 0 },
-    { mensagem: 'ola', numero: 1},
-    { mensagem: 'hi', numero: 2},
-    { mensagem: 'hello', numero: 3},
-]
+let produtos = [
+    {
+        nome: "CARTAO1",
+        valor: "R$9,99",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO2",
+        valor: "R$14,50",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO3",
+        valor: "R$25,00",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO4",
+        valor: "R$34,90",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO5",
+        valor: "R$49,99",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO6",
+        valor: "R$59,00",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO7",
+        valor: "R$79,99",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO8",
+        valor: "R$99,90",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO9",
+        valor: "R$129,50",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO10",
+        valor: "R$199,99",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO11",
+        valor: "R$299,00",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    },
+    {
+        nome: "CARTAO12",
+        valor: "R$49,99",
+        img: 'https://services.meteored.com/img/article/o-misterio-da-antimateria-solucionado-novo-experimento-comprova-como-a-antimateria-interage-1695942655165_1280.png'
+    }
+];
 
-app.post('/falas', (rep, res) => {
-    const mensagem = req.mensagem;
-    console.log(mensagem)
-    res.status(201).json({ mesagem: 'fununcio'});
+app.get('/produtos', (req, res) => {
+    res.status(200).json({ produtos });
+    console.log('deu certin')
+});
 
-})
-
-app.length('/falas', (req, res) => {
-    res.status(200).json({ vetor });
-    console.log('oi');
-})
-
-app.length('/', (req, res) => {
-    res.status(200).json({ mensagem: 'Olá mundo' })
-        console.log('oi')
+app.post('/produtos', (req, res) => {
+    const { nome, valor, img } = req.body;
     
-})
+    produtos.push({ nome: nome, valor: valor, img : img });
+    console.log(vetor);
+    res.status(201).json({ mensagem: 'é POST' });
 
-app.listen(porta,() =>{
-    console.log (`sevidor rodando ma porta ${porta}`)
-})
+});
 
+app.delete('/fds', (req, res) => {
+    const numero = req.body.numero;
+    produtos.splice(numero, 1);
+    console.log(vetor);
+    res.status(201).json({ mensagem: 'é DELETE' });
 
+});
 
+app.put('/fds', (req, res) => {
+    const numero = req.body.numero;
+    const mensagem = req.body.mensagem;
+    produtos[numero], mensagem = mensagem;
+    console.log(vetor);
+    res.status(201).json({ mensagem: 'é PUT' });
 
+});
 
-
-
-
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
